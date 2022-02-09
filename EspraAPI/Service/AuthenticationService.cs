05f4e792314fac828a26ea86b85ab9f8c99c0a46
Boris <Borismulder@outlook.com>
2022-02-09 18:47:15 +0100

    Create AuthenticationService.cs

---
+++ EspraAPI/Service/AuthenticationService.cs
@@ -0,0 +1,51 @@
+﻿using EspraAPI.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace EspraAPI.Service
+{
+    public class AuthenticationService
+    {
+        UserManager<AuthenticationUser> UserManager { get; set; }
+        public AuthenticationService(UserManager<AuthenticationUser> userManager)
+        {
+            UserManager = userManager;
+        }
+
+        public async Task<LoginResponse> Login(LoginModel loginModel)
+        {
+            if(!loginModel.IsValid())
+                return new LoginResponse 
+                {
+                    Success = false,
+                    Message = "InValid form"
+                };
+
+            var user = await UserManager.FindByEmailAsync(loginModel.UserName);
+
+            if (user == null)
+                return new LoginResponse 
+                { 
+                    Success = false, 
+                    Message = "User not found" 
+                };
+
+            var loginResult = await UserManager.CheckPasswordAsync(user, loginModel.Password);
+
+            if (loginResult)
+            {
+                return new LoginResponse
+                {
+                    Success = true,
+                    Message = "Authenticated successfully",
+                    Token = JWT.GenerateJWT(user.UserName, await UserManager.GetRolesAsync(user))
+                };
+            }
+
+            return new LoginResponse 
+            {
+                Success = false,
+                Message = "Failed to login"
+            };
+        }
+    }
+}
