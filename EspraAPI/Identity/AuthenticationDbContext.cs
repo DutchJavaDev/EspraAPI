983fc7f44dbb274b222fa85b61227d2fa48e9206
Boris <Borismulder@outlook.com>
2022-02-19 00:57:15 +0100

    Create AuthenticationDbContext.cs

---
+++ EspraAPI/Identity/AuthenticationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EspraAPI.Identity
+{
+    public class AuthenticationDbContext : IdentityDbContext<AuthenticationUser>
+    {
+        public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options) : base(options)
+        { 
+        }
+    }
+}
