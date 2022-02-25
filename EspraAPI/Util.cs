d0322bb169f27ed10c290f848a422d28229eb288
Boris <Borismulder@outlook.com>
2022-02-25 23:29:42 +0100

    Update Util.cs

--- EspraAPI/Util.cs
+++ EspraAPI/Util.cs
@@ -15,6 +15,15 @@ namespace EspraAPI
             "Web", // <- read
         };
 
+        // Move this to a external file to be more dynamic ?
+        // No need to restart backend when file changes
+        public static string[] FILES_EXTENSIONS = {
+            ".png",
+            ".jpg",
+            ".jpeg",
+            ".txt",
+        };
+
         private static string? ValidIssuer;
         private static string? ValidAudience;
         private static string? Secret;
@@ -43,11 +52,42 @@ namespace EspraAPI
             var token = new JwtSecurityToken(
                    issuer: ValidIssuer,
                    audience: ValidAudience,
-                   expires: DateTime.Now.AddHours(3),
+                   expires: DateTime.Now.AddSeconds(30),
                    claims: authenticationClaims,
                    signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256));
 
             return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public static string GetUser(string token)
+        {
+            if (string.IsNullOrEmpty(token))
+                return string.Empty;
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            try
+            {
+                tokenHandler.ValidateToken(token, new TokenValidationParameters 
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = ValidAudience,
+                    ValidIssuer = ValidIssuer,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Secret ?? string.Empty)),
+
+                    // set clockskew to zero so tokens expire exactly at token expiration time (instead of 5 minutes later)
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validationToken);
+
+                var parsedJwt = (JwtSecurityToken) validationToken;
+
+                return parsedJwt.Claims.First(i => i.Type == ClaimTypes.Name).Value;
+            }
+            catch
+            {
+                return string.Empty;
+            }
         }
     }
 }
