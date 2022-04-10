39a135fe90489d508301e1fbccf6bdc98b8e3f49
Boris <Borismulder@outlook.com>
2022-04-10 16:01:27 +0200

    dasd

--- EspraAPI/Service/Esp32StorageService.cs
+++ EspraAPI/Service/Esp32StorageService.cs
@@ -13,7 +13,7 @@ namespace EspraAPI.Service
         public Esp32StorageService()
         {
             sessionFactory = Fluently.Configure()
-                .Database(MySQLConfiguration.Standard.ConnectionString("Server=localhost;Uid=root;Database=esp32snapshotdb;Pwd=Kwende1995!;"))
+                .Database(MySQLConfiguration.Standard.ConnectionString("Server=localhost;Uid=username;Database=esp32snapshotdb;Pwd=password;"))
                 .Mappings(i => i.FluentMappings.AddFromAssemblyOf<Esp32ModelMapping>())
                 .ExposeConfiguration(cfg => new SchemaUpdate(cfg).Execute(true, true))
                 .BuildSessionFactory();
