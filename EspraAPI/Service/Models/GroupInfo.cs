73734c105e5429644a1a211ac2cb9b2039dee692
Boris <Borismulder@outlook.com>
2022-04-12 18:09:10 +0200

    Create GroupInfo.cs
    track a collection

---
+++ EspraAPI/Service/Models/GroupInfo.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace EspraAPI.Service.Models
+{
+    public class GroupInfo
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
+
+        public string GroupName { get; set; } = string.Empty;
+
+        public IList<string> JsonIds { get; set; } = new List<string>();
+
+        public IList<string> FileIds { get; set; } = new List<string>();
+    }
+}
