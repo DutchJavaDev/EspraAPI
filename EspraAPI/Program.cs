850359a7e6d04eec1f3006f097a966a16aea4df9
Boris <Borismulder@outlook.com>
2022-05-03 14:05:54 +0200

    Update Program.cs
    moved mapping endpoints to separate handles

--- EspraAPI/Program.cs
+++ EspraAPI/Program.cs
@@ -3,18 +3,13 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using EspraAPI.Identity;
 using EspraAPI.Service;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
-using Microsoft.AspNetCore.Authorization;
 using Sentry;
 using MongoDB.Driver;
 using EspraAPI;
 using static EspraAPI.Configuration.ContentMiddleware;
 using EspraAPI.Handlers;
-using static EspraAPI.Handlers.JsonHandler;
-using static EspraAPI.Handlers.AuthHandler;
-using static EspraAPI.Handlers.FileHandler;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -113,107 +108,44 @@ app.MapPost("api/post/json/{group}", JsonHandler.PostJson)
 .Produces(StatusCodes.Status400BadRequest)
 .WithDisplayName("New jsondata entry");
 
-
-app.MapGet("api/get/json/groupId/{group}", JsonHandler.GetJsonById)
+app.MapGet("api/get/json/groupId/{group}", JsonHandler.GetJsonByGroupId)
 .Accepts<string>(json)
 .Produces<IList<JsonData>>(StatusCodes.Status200OK)
 .WithDisplayName("Get all data grouped by groupId");
 
-app.MapGet("api/get/json/id/{id}", [Authorize(Roles = "Admin")] async (string id, CancellationToken token, JsonService jsonService) =>
-{
-    var result = await jsonService.GetByIdAsync(id, token);
-
-    return ! (result == null) ? Results.Ok(result) : Results.BadRequest();
-}).Accepts<string>(json)
+app.MapGet("api/get/json/id/{id}", JsonHandler.GetJsonById).
+ Accepts<string>(json)
 .Produces<JsonData>(StatusCodes.Status200OK)
 .Produces(StatusCodes.Status400BadRequest)
 .WithDisplayName("Get a jsondata by its documentId");
 
-
-app.MapPost("api/update/json/{id}", [Authorize(Roles = "Admin")] async (string id, [FromBody] dynamic ndata, JsonService jsonService, CancellationToken token) =>
-{
-    return (await jsonService.UpdateByIdAsync(id, ndata, token)) ? Results.Ok() : Results.BadRequest();
-}).Accepts<dynamic>(json)
+app.MapPost("api/update/json/{id}", JsonHandler.UpdateJsonById)
+.Accepts<dynamic>(json)
 .Produces(StatusCodes.Status200OK)
 .Produces(StatusCodes.Status400BadRequest)
 .WithDisplayName("Update an existing jsondata by its Id");
 
-
-app.MapDelete("api/delete/json/{id}", [Authorize(Roles = "Admin")] async (string id, CancellationToken token, JsonService jsonService) =>
-{
-    return (await jsonService.DeleteByIdAsync(id, token)) ? Results.Ok() : Results.BadRequest();
-}).Accepts<dynamic>(json)
+app.MapDelete("api/delete/json/{id}", JsonHandler.DeleteJsonById)
+ .Accepts<dynamic>(json)
 .Produces(StatusCodes.Status200OK)
 .Produces(StatusCodes.Status400BadRequest)
 .WithDisplayName("Delete jsondata by its Id");
 
-app.MapPost("api/post/document/{group}", [Authorize(Roles = "Admin")]  async (string group, HttpRequest request, CancellationToken token, FileService fileService) =>
-{
-    if (!request.HasFormContentType)
-        return Results.BadRequest();
-
-    var form = await request.ReadFormAsync();
-
-    var document = form.Files.First(i => i != null && i.Length > 0);
-
-    if (document is null)
-        return Results.BadRequest("Empty request");
-
-    var documentExtension = Path.GetExtension(document.FileName);
-    
-    if(!Util.DOCUMENT_EXTENSIONS.Contains(documentExtension))
-        return Results.BadRequest("Unsupported file");
-
-    using var stream = new MemoryStream();
-
-    document.CopyTo(stream);
-
-    return await fileService.AddAsync(group, documentExtension, stream.ToArray(), token) ? Results.Ok() : Results.BadRequest();
-}).Accepts<IFormFile>(formFile)
+app.MapPost("api/post/document/{group}", FileHandler.PostDocument)
+.Accepts<IFormFile>(formFile)
 .Produces(StatusCodes.Status200OK)
 .Produces(StatusCodes.Status400BadRequest)
 .WithDisplayName("Upload a document");
 
-app.MapPost("api/post/image/{group}", [Authorize(Roles = "Admin")] async (string group, HttpRequest request, CancellationToken token, FileService fileService) =>
-{
-    if (!request.HasFormContentType)
-        return Results.BadRequest();
-
-    var form = await request.ReadFormAsync();
-
-    var image = form.Files.First(i => i != null && i.Length > 0);
-
-    if (image is null)
-        return Results.BadRequest("Empty request");
-
-    var imageExtension = Path.GetExtension(image.FileName);
-
-    if (!Util.IMAGE_EXTENSIONS.Contains(imageExtension))
-        return Results.BadRequest("Unsupported file");
-
-    using var stream = new MemoryStream();
-
-    image.CopyTo(stream);
-
-    return await fileService.AddAsync(group, imageExtension, stream.ToArray(), token) ? Results.Ok() : Results.BadRequest();
-}).Accepts<IFormFile>(formFile)
+app.MapPost("api/post/image/{group}", FileHandler.PostImage)
+.Accepts<IFormFile>(formFile)
 .Produces(StatusCodes.Status200OK)
 .Produces(StatusCodes.Status400BadRequest)
 .WithDisplayName("Upload a image");
 
-app.MapGet("api/get/document/{id}", async (string id, FileService fileService, CancellationToken token) => 
-{
-    var documentData = await fileService.GetDocumentByIdAsync(id, token);
+app.MapGet("api/get/document/{id}", FileHandler.GetDocumentById);
 
-    return Results.File(documentData.Item1,documentData.Item2);
-});
-
-app.MapGet("api/get/image/{id}", async (string id, FileService fileService, CancellationToken token) =>
-{
-    var imageData = await fileService.GetImageByIdAsync(id, token);
-
-    return Results.File(imageData.Item1, imageData.Item2);
-});
+app.MapGet("api/get/image/{id}", FileHandler.GetImageById);
 #endregion
 
 
